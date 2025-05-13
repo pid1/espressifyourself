56d58bd04656b0a7e1550e4bb080a731b19f62a7
Jonathan Roemer <win@cygnusstrategy.com>
2025-05-13 06:24:30 -0500

    Move site to static

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # espressifyourself
 
-My personal homepage running on an ESP32-S2 (AdaFruit MagTag) via CircuitPython
+Website running on an ESP32-S2 (AdaFruit MagTag) via CircuitPython.
 
 See it in action [here](https://admittedly-neutral-mantis.ngrok-free.app/).
 
--- code.py
+++ code.py
@@ -11,28 +11,14 @@ wifi.radio.connect(os.getenv("CIRCUITPY_WIFI_SSID"), os.getenv("CIRCUITPY_WIFI_P
 pool = socketpool.SocketPool(wifi.radio)
 server = Server(pool, "/static", debug=True)
 
-def webpage():
-    html = f"""
-    <!DOCTYPE html>
-    <html>
-    hello world!
-    <br><br>
-    this is a development server running on an ESP32-S2 via CircuitPython
-    <br><br>
-    brought to you by <a href="https://pid1.space">pid1</a>
-    <br><br>
-    <img src="fastweb.gif" alt="GIF explaining that the website is fast">
-    </html>
-    """
-    return html
-
 @server.route("/")
 def base(request: Request):
-    return Response(request, f"{webpage()}", content_type='text/html')
-
-@server.route("/static")
-def base(request: Request):
-    return Response(request, f"{webpage()}", content_type='text/html')
+    try:
+        with open("/static/index.html", "r") as file:
+            html_content = file.read()
+        return Response(request, html_content, content_type='text/html')
+    except Exception as e:
+        return Response(request, f"Error loading file: {e}", content_type='text/plain')
 
 server.start()
 
@@ -41,4 +27,3 @@ while True:
         server.poll()  # Process incoming requests
     except Exception as e:
         print("Error:", e)
-
